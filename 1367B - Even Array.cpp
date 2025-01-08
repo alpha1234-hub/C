2e89c2703ac4ed7b2e1e190ae5369da232faa18a
Sowad Rahman <85169265+sowad223@users.noreply.github.com>
2025-01-09 02:11:43 +0600

    Update 1367B - Even Array.cpp

--- 1367B - Even Array.cpp
+++ 1367B - Even Array.cpp
@@ -1,35 +1,31 @@
-
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-
-int main()
-{
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        int a[n];
-        for(int i=0; i<n; i++)
-        {
-            cin>>a[i];
-        }
-        int od = 0, ev = 0;
-        for(int i=0; i<n; i++)
-        {
-            if(i%2 != a[i]%2)
-            {
-                if(a[i]%2 == 1)
-                od++;
-                else 
-                ev++;
+        cin >> n;
+        int odd_mismatch = 0, even_mismatch = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num;
+            cin >> num;
+            // Check if the parity of index and value mismatch
+            if (i % 2 != num % 2) {
+                if (num % 2 == 1)
+                    odd_mismatch++;
+                else
+                    even_mismatch++;
             }
         }
-        if(od == ev)
-        cout<<od<<endl;
+
+        // Output the result
+        if (odd_mismatch == even_mismatch)
+            cout << odd_mismatch << endl;
         else
-        cout<<-1<<endl;
+            cout << -1 << endl;
     }
+    return 0;
 }
